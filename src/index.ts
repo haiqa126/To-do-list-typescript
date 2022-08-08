0550fea3b67e08139f2ff5e29dc2d582a9abba33
Haiqa Ashraf <ashrafhaiqa537@gmail.com>
2022-08-08 22:24:03 +0100

    append problem

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+//console.log("hi") -works fine 
 import {v4 as uuidV4} from "uuid";
 
 
@@ -9,32 +10,43 @@ type Task={id:string,
 const list=document.querySelector<HTMLUListElement>('#list');
 const form=document.querySelector<HTMLFormElement>('#new-task-form') as HTMLFormElement |null;
 const input=document.querySelector<HTMLInputElement>('#new-task-title');
-const tasks:Task[] =loadTasks()
-tasks.forEach(addListItem)
+const tasks:Task[] =loadTasks();
+tasks.forEach(addListItem);
 
-form?.addEventListener("submi",e=>{
-e.preventDefault()
+form?.addEventListener("submit",e=>{
+e.preventDefault();
+console.log("form was submitted")
 
-if (input?.value==="" || input?.value===null) return
+if (input?.value=="" || input?.value==null) return
  const newTask: Task={
   id: uuidV4(),
-  title: input?.value,
+  title: input.value,
   completed:false,
   createdAt: new Date()
+
  }
+ //console.log("the task is:")
+console.log(newTask);
 
  tasks.push(newTask);
  saveTasks();
 
-addListItem(newTask)
-input?.value=""
+addListItem(newTask);
+input.value="";
 
-})
+});
+
+//Form submission has ended
+
+
+
 
 function addListItem(task:Task){
  const item=document.createElement("li");
  const label=document.createElement("label");
  const checkbox=document.createElement("input");
+
+
  checkbox.addEventListener("change",()=>{
   task.completed=checkbox.checked;
  saveTasks()
@@ -44,21 +56,25 @@ function addListItem(task:Task){
 
  checkbox.type="checkbox";
  checkbox.checked=task.completed;
+ label.append(checkbox, task.title);
+ item.append(label);
+ list?.appendChild(item);
 
- label.append(checkbox, task.title)
- item.append(label)
- list?.append(item)
+ 
 
 }
 
 function saveTasks(){
-  localStorage.setItem("Tasks",JSON.stringify(tasks))
+  localStorage.setItem("Tasks",JSON.stringify(tasks));
+  console.log("task saved to local storage");
 }
 
 function loadTasks():Task[]{
 const taskJson=localStorage.getItem("Tasks")
-if(taskJson==null) return []
-return JSON.parse(taskJson)
+if(taskJson==null) return [];
+console.log("i loaded the tasks from the local storage");
+return JSON.parse(taskJson);
+
 
 
 }
